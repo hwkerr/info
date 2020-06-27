93074b71e0ad728659e33d0fea6f473de51092aa
Harrison Kerr <harrisonwkerr@gmail.com>
2020-06-27 14:53:10 -0400

    Add portfolio website

---
+++ me/index.js
@@ -0,0 +1,15 @@
+//$(".skill").fadeOut();
+
+$("#no-selection").show();
+
+// function hideSkills() {
+//   $(".skill").slideUp();
+// }
+
+$(".skill-btn").click(showSkill);
+
+function showSkill(event) {
+  let sectionID = $(this).attr("data-section");
+  $(".skill").not("#" + sectionID).hide(); // hide others
+  $("#" + sectionID).fadeToggle(); // toggle clicked
+}
